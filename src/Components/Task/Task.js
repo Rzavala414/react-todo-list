1aa8ae409b61ab2d7826bb09e2f3c44fc429c3ae
Roger Zavala <rogeliozavala15@gmail.com>
2021-03-09 19:11:05 -0800

    able to get user input and display it

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Task.css';
 
+// Creates a task
 export default class Task extends React.Component{
 
     render(){
 
         return(
             <div className='task'>
-                {/* layout task here */}
+                {this.props.task}
                 <span>X</span>
             </div>
         )
--- src/Components/Tasksbox/Tasksbox.js
+++ src/Components/Tasksbox/Tasksbox.js
@@ -2,13 +2,34 @@ import React from 'react';
 import TaskContainer from '../TaskContainer/TaskContainer';
 import './Tasksbox.css';
 
+// Displays the input for users and container of tasks
 export default class Tasksbox extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            task: 'Enter Task'
+        }
+
+        this.onClick = this.onClick.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onClick(){
+        this.props.getTask(this.state.task)
+    }
+
+    onChange(event){
+        let task = event.target.value;
+        this.setState({task})
+    }
 
     render(){
         return(
             <div className="container">
-                <input type='text' placeholder="Add Task" ></input>
-                <TaskContainer/>
+                <input type='text' placeholder={this.state.task} onChange={this.onChange} ></input>
+                <button onClick={this.onClick}>Add Task</button>
+                
+                <TaskContainer tasks={this.props.tasks}/>
             </div>
         )
     }
